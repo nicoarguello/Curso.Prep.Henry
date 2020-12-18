bdf5dadce5bcdde3d0e0e24925b4f81b54fb8403
Nicolas <nicoarguello36@gmail.com>
2020-12-18 15:40:31 -0300

    Tarea 6

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,6 +1,6 @@
 // No cambies los nombres de las funciones.
 
-function crearUsuario() {
+function crearUsuario () {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
@@ -9,13 +9,29 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+class Usuario  
+
+    {
+      constructor (opciones)
+      {
+      this.usuario= opciones.usuario,
+      this.nombre= opciones.nombre,
+      this.email= opciones.email,
+      this.password= opciones.password
+      }
+    }
+    Usuario.prototype.saludar= function() {return ('Hola, mi nombre es '+ this.nombre )   }
+    
+  
+  
+  return Usuario
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+Constructor.prototype.saludar= function (){ return 'Hello World!'}
 }
 
 function agregarStringInvertida() {
@@ -24,7 +40,14 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  String.prototype.reverse= function () {
+    var s=''
+    for(var i=this.length -1; i>=0; i--)
+    {
+    s=s+ this.charAt(i);
+    }
+          return s;
+  }
 }
 
 // No modificar nada debajo de esta línea
